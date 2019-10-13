ed6d974fa07ba590549185c169c46aceb876ad87
Sandeep MK <Sandeepmknair@gmail.com>
2019-10-13 18:00:04 +0530

    added sql script to create auth tables

--- src/test/java/com/inventorymanagement/InventoryManagerAppTests.java
+++ src/test/java/com/inventorymanagement/InventoryManagerAppTests.java
@@ -1,4 +1,4 @@
-package com.webecncyclop.demo;
+package com.inventorymanagement;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class InventoryManagerAppTests {
 
 	@Test
 	public void contextLoads() {
