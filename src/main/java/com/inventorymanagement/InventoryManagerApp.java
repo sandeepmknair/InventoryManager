ec3deb5f376fa159af4a258aed68f661b51b0772
Sandeep MK <Sandeepmknair@gmail.com>
2019-10-13 17:08:43 +0530

    initial commit

---
+++ src/main/java/com/inventorymanagement/InventoryManagerApp.java
@@ -0,0 +1,12 @@
+package com.inventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerApp.class, args);
+	}
+}
